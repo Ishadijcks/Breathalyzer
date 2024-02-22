7412bb122e0da7a6fab3f24121131e0259872da8
Isha Dijcks <ishadijcks@gmail.com>
2024-02-22 20:47:42 +0100

    Allow adding private key directly

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ const webhookSecret = process.env.WEBHOOK_SECRET;
 const privateKeyPath = process.env.PRIVATE_KEY_PATH;
 
 // This reads the contents of your private key file.
-const privateKey = fs.readFileSync(privateKeyPath, "utf8");
+const privateKey = process.env.PRIVATE_KEY ?? fs.readFileSync(privateKeyPath, "utf8");
 
 // This creates a new instance of the Octokit App class.
 const app = new App({
